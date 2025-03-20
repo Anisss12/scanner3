5a16d81881b134d775b2038a267609bb1f7ae701
Anisss12 <151550400+Anisss12@users.noreply.github.com>
2025-03-20 20:13:40 +0530

    Create loading.js

---
+++ src/app/loading.js
@@ -0,0 +1,15 @@
+// components/LoadingScreen.js
+"use client";
+import React from "react";
+import styles from "./LoadingScreen.module.css";
+
+const LoadingScreen = () => {
+  return (
+    <div className={styles.loadingContainer}>
+      <div className={styles.spinner}></div>
+      <h2 className={styles.text}>Loading...</h2>
+    </div>
+  );
+};
+
+export default LoadingScreen;
