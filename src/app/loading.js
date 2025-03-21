5609ef52b7c64f7ac12f8ecf5309027a4a1ef548
Anisss12 <151550400+Anisss12@users.noreply.github.com>
2025-03-21 15:08:46 +0530

    Update loading.js

--- src/app/loading.js
+++ src/app/loading.js
@@ -1,14 +1,13 @@
 // components/LoadingScreen.js
 "use client";
 import React from "react";
-import styles from "page.module.css"
+
 
 
 const LoadingScreen = () => {
   return (
-    <div className={styles.loadingContainer}>
-      <div className={styles.spinner}></div>
-      <h2 className={styles.text}>Loading...</h2>
+    <div>
+   Loading...
     </div>
   );
 };
